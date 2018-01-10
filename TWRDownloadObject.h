b9aa9674b35cfa8e9fdf55fec5182e8431ccd1da
Dhaval Patel <dhaval.patel@agileinfoways.com>
2018-01-10 14:14:10 +0530

    no message

--- TWRDownloadObject.h
+++ TWRDownloadObject.h
@@ -20,7 +20,7 @@ typedef void(^TWRDownloadCompletionBlock)(BOOL completed);
 @property (copy, nonatomic) TWRDownloadRemainingTimeBlock remainingTimeBlock;
 
 @property (strong, nonatomic) NSURLSessionDownloadTask *downloadTask;
-@property (copy, nonatomic) NSString *fileName;
+@property (strong, nonatomic) NSString *fileName;
 @property (copy, nonatomic) NSString *friendlyName;
 @property (copy, nonatomic) NSString *directoryName;
 @property (copy, nonatomic) NSDate *startDate;
